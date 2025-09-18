950711d7376ba947a3a22b8a0c636bd4a1512697
Lorenzo Fontana <fontanalorenz@gmail.com>
2025-09-18 11:58:21 +0200

    chore: avoid downloading the musl license
    This is done to avoid that the build fails when the musl website goes
    down.

--- check_musl.cmake
+++ check_musl.cmake
@@ -29,27 +29,14 @@ function(fetch_musl_license_if_needed)
     if(CMAKE_IS_USING_MUSL)
         message(STATUS "musl libc detected, configuring license installation.")
 
-        set(MUSL_LICENSE_URL "https://git.musl-libc.org/cgit/musl/plain/COPYRIGHT")
-        set(DOWNLOAD_LOCATION "${CMAKE_BINARY_DIR}/musl.COPYRIGHT")
-        install(FILES ${DOWNLOAD_LOCATION} DESTINATION ${CMAKE_INSTALL_DOCDIR}/third_party RENAME musl.COPYRIGHT)
+        set(LOCAL_MUSL_LICENSE "${CMAKE_SOURCE_DIR}/musl.COPYRIGHT")
 
-        if (NOT EXISTS ${DOWNLOAD_LOCATION})
-            message(STATUS "Downloading musl license from ${MUSL_LICENSE_URL} to ${DOWNLOAD_LOCATION}.")
+        if(EXISTS ${LOCAL_MUSL_LICENSE})
+            install(FILES ${LOCAL_MUSL_LICENSE} DESTINATION share/doc/fusion-snapshot)
+            message(STATUS "Using local musl license from ${LOCAL_MUSL_LICENSE}")
         else()
-            message(STATUS "musl license already exists at ${DOWNLOAD_LOCATION}, skipping download.")
-            return()
+            message(WARNING "musl libc detected but local musl.COPYRIGHT file not found at ${LOCAL_MUSL_LICENSE}")
         endif()
-
-        file(DOWNLOAD ${MUSL_LICENSE_URL} ${DOWNLOAD_LOCATION} SHOW_PROGRESS STATUS download_status)
-        
-
-        list(GET download_status 0 download_result)
-        if(NOT download_result EQUAL 0)
-            list(GET download_status 1 error_msg)
-            message(FATAL_ERROR "Failed to download musl license from ${MUSL_LICENSE_URL}. Error: ${error_msg}")
-        endif()
-
-        message(STATUS "musl license downloaded successfully.")
     else()
         message(STATUS "musl libc not detected. Skipping license installation.")
     endif()
